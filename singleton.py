d63a3af8c26bfc1134f16442856563a568e00a6b
hawkli <hawkli@yespowering.com>
2018-03-22 17:39:55 +0800

    单例模式

---
+++ singleton.py
@@ -0,0 +1,37 @@
+"""
+单例模式
+
+意图：保证一个类仅有一个实例，并提供一个访问它的全局访问点。
+
+主要解决：一个全局使用的类频繁地创建与销毁。
+
+何时使用：当您想控制实例数目，节省系统资源的时候。
+
+如何解决：判断系统是否已经有这个单例，如果有则返回，如果没有则创建。
+
+关键代码：构造函数是私有的。
+"""
+
+class Singleton(type):
+    def __init__(self, *args, **kwargs):
+        self.__instance = None
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        if self.__instance is None:
+            self.__instance = super().__call__(*args, **kwargs)
+            return self.__instance
+        else:
+            return self.__instance
+
+# Example
+class Spam(metaclass=Singleton):
+    def __init__(self):
+        print('Creating Spam')
+
+if __name__ == '__main__':
+    a = Spam()
+    b = Spam()
+    a is b
+    c = Spam()
+    c is a
