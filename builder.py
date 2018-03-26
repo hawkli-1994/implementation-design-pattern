6dfab4fe7a724b201bd950dde580e26afc926b14
LiKeran <969543965@qq.com>
2018-03-26 20:24:15 +0800

    builder

--- builder.py
+++ builder.py
@@ -3,3 +3,29 @@
 
 一个 Builder 类会一步一步构造最终的对象。该 Builder 类是独立于其他对象的。
 """
+
+
+"""
+在这里，我们定义一套肯德基套餐的制造过程，用来体现建造者模式。
+"""
+from abc import ABCMeta, abstractmethod
+
+
+class Builder:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def build_burger(self)
+
+
+class Diretor:
+    """指挥者"""
+    pass
+
+
+
+
+class Client:
+    pass
+
+
